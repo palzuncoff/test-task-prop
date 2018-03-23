942a49b85ec053d2ef4b648a6cb031a52876ef9a
Dmitrii Lazucov <palzuncoff@MacBook-Dmitrii.local>
2018-03-23 13:57:11 +0200

    Add loader

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { loadPosts, sortPosts } from '../AC';
 import { SORT_BY_ID, SORT_BY_USER, SORT_BY_TITLE, SORT_BY_BODY} from '../constants'
 import { sortedPosts, getPostLoaded, getPostsLoading } from '../selectors';
+import Loader from './loader';
 import './App.css';
 
 class App extends Component {
@@ -14,36 +15,38 @@ class App extends Component {
         }
     }
 
-  handlerSortSelect = culumn => {
-      this.props.sortPosts(culumn)
-  }
+    handlerSortSelect = culumn => {
+        this.props.sortPosts(culumn)
+    }
 
-  render() {
-      const { posts } = this.props;
-      const renderTable = post => (
-          <tr key={post.id}>
-              <td>{post.id}</td>
-              <td>{post.userId}</td>
-              <td>{post.title}</td>
-              <td>{post.body}</td>
-          </tr>);
+    render() {
+        const { posts, loading } = this.props;
+        const renderTable = post => (
+            <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.userId}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+            </tr>);
 
-      return (
-          <table>
-              <thead>
-                  <tr>
-                      <th><span onClick={() => { this.handlerSortSelect(SORT_BY_ID); }}>ID</span></th>
-                      <th><span onClick={() => { this.handlerSortSelect(SORT_BY_USER); }}>User</span></th>
-                      <th><span onClick={() => { this.handlerSortSelect(SORT_BY_TITLE); }}>Title</span></th>
-                      <th><span onClick={() => { this.handlerSortSelect(SORT_BY_BODY); }}>Body</span></th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {posts.map(renderTable)}
-              </tbody>
-          </table>
-      );
-  }
+        if (loading) return <Loader />
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th onClick={() => { this.handlerSortSelect(SORT_BY_ID); }}>ID</th>
+                        <th onClick={() => { this.handlerSortSelect(SORT_BY_USER); }}>User</th>
+                        <th onClick={() => { this.handlerSortSelect(SORT_BY_TITLE); }}>Title</th>
+                        <th onClick={() => { this.handlerSortSelect(SORT_BY_BODY); }}>Body</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {posts.map(renderTable)}
+                </tbody>
+            </table>
+        );
+    }
 }
 
 App.propTypes = {
---
+++ src/components/loader/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Loader() {
+    return (
+        <h2>loading...</h2>
+    )
+}
+
+export default Loader;
